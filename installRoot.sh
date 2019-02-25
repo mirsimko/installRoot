00b37ef85cfc157d8c579208e2b9c047803ea1ff
svihra <psvihra@gmail.com>
2019-02-25 21:07:01 +0100

    updated to use getopts

--- installRoot.sh
+++ installRoot.sh
@@ -1,140 +1,112 @@
-#!/bin/bash
+#!/bin/sh
 
 #------------------------------------------------------------------------
 # - made by Peter Svihra, FNSPE CTU in Prague
-# - the script needs at least 1 parameter (nCores)
-# - first parameter is number of cores for faster execution
-# - second parameter is direct link to download the root file with .tar.gz
+# - c parameter is number of cores for faster execution
+# - w parameter is direct link to download the root file with .tar.gz
 # extension (automatically 6.08.06, possible any version supporting cmake)
-# - third parameter is shell type used (automatically bash, possible zsh)
+# - s parameter is shell type used (automatically bash, possible zsh)
 #------------------------------------------------------------------------
 
-if [ $# -eq 0 ]; then
-    echo "#########################################################################################";
-    echo "Add number of the used cores, direct link to download and shell type (automatically bash)";
-    echo "bash installRoot.sh nCores directWebLink shellType";
-    echo "#########################################################################################";
-    exit
-fi
+CORES=1
+WEB=https://root.cern.ch/download/root_v6.08.06.source.tar.gz
+SHELL=bash
 
-if [ $# -ge 2 ]; then
-    echo "Will download file from $2";
-    WEB=$2;
-else
-    WEB=https://root.cern.ch/download/root_v6.08.06.source.tar.gz;
-fi
+while getopts ":hc:w:s:" opt; do
+    case "${opt}" in
+        h) echo "./installRoot.sh -c nCores -w directWebLink -s shellType";;
+        c) CORES=${OPTARG};;
+        w) WEB=${OPTARG};;
+        s) SHELL=${OPTARG};;
+        \?) echo "Invalid option: -$OPTARG";;
+        :) echo "Option -$OPTARG requires an argument."; exit 1;;
+    esac
+done
+
+echo "#########################################################################################"
+echo "Downloading file from: $WEB"
+echo "Using shell: $SHELL"
+echo "Number of cores: $CORES"
 
 NAME=$WEB;
 NAME=${NAME/https\:\/\/root\.cern\.ch\/download\//};
 NAME=${NAME/\.source\.tar\.gz/};
-
-if [ $# -eq 3 ]; then
-    echo "#########################################################################################";
-    echo "Using $3 shell";
-    echo "#########################################################################################";
-    SHELL=$3;
-else
-    SHELL=bash;
-fi
-
 FILE=~/Downloads/$NAME.tar.gz;
 
 if [ -f $FILE ]; then
     echo "#########################################################################################";
-    echo $FILE already exists ... skipping download
-    echo "#########################################################################################";
+    echo "$FILE already exists -> skipping download"
 else
     if ! wget --show-progress --output-document=$FILE $WEB ; then
-	echo "#########################################################################################";
-	echo "Could not download the file $WEB";
-	echo "#########################################################################################";
-	exit;
+        echo "#########################################################################################";
+        echo "Could not download the file $WEB";
+        exit;
     else
-	echo "#########################################################################################";
-	echo "File successfuly downloaded";
-	echo "#########################################################################################";
-  fi
-fi
-
-if [ $# -eq 0 ]; then
-    echo "#########################################################################################";
-    echo "Using only one core";
-    echo "#########################################################################################";
-    CORES=1;
-else
-    if [[ $1 =~ ^-?[0-9]+$ ]]; then
-        CORES=$1;
-    else
-		echo "#########################################################################################";
-		echo "Not valid cores count, using only one";
-		echo "#########################################################################################";
-		CORES=1;
+      	echo "#########################################################################################";
+      	echo "File successfuly downloaded";
     fi
 fi
 
+if [[ $CORES =~ ^-?[0-9]+$ ]]; then
+    CORES=$1;
+else
+  	echo "#########################################################################################";
+  	echo "Not valid cores count, using only one";
+  	CORES=1;
+fi
+
 sudo echo ;
-sudo apt-get --ignore-missing --yes --force-yes install qtcreator git dpkg-dev gcc g++ make cmake binutils libx11-dev libxpm-dev libxft-dev libxext-dev libqt4-dev python python-dev lzma-dev libgl2ps-dev libxml2-dev openssl;
+sudo apt-get --ignore-missing --yes --allow install qtcreator git dpkg-dev gcc g++ make cmake binutils libx11-dev libxpm-dev libxft-dev libxext-dev libqt4-dev python python-dev lzma-dev libgl2ps-dev libxml2-dev openssl;
 
 if [ ! -d /opt/$NAME/root-build ]; then
 
     if sudo mkdir -p /opt/$NAME/root-build; then
         echo "#########################################################################################";
-		echo "build dir - created";
-        echo "#########################################################################################";
+        echo "build dir - created";
     else
         echo "#########################################################################################";
-		echo "build dir - creation failed";
-		echo "#########################################################################################";
-		exit;
+	      echo "build dir - creation failed";
+	      exit;
     fi
 else
     echo "#########################################################################################";
     echo "build dir - already exists";
-    echo "#########################################################################################";
 fi
 
 
 if [ ! -d /opt/$NAME/source ]; then
     if sudo mkdir -p /opt/$NAME/source; then
-		echo "#########################################################################################";
+		    echo "#########################################################################################";
         echo "source dir - created";
-        echo "#########################################################################################";
-	else
-		echo "#########################################################################################";
+    else
+    		echo "#########################################################################################";
         echo "source dir - creation failed";
-		echo "#########################################################################################";
         exit;
     fi
 else
 		echo "#########################################################################################";
-        echo "source dir - already exists";
-		echo "#########################################################################################";
+    echo "source dir - already exists";
 fi
 
 if sudo tar -C /opt/$NAME/source --strip-components 1 -zxf $FILE; then
     echo "#########################################################################################";
     echo "unpack - successful";
-    echo "#########################################################################################";
 else
     echo "#########################################################################################";
     echo "unpack - failed";
-    echo "#########################################################################################";
     exit;
 fi
 
 SOURCE=/opt/$NAME/source
 echo "#########################################################################################";
 echo "source dir - "$SOURCE;
-echo "#########################################################################################";
 
 cd /opt/$NAME/root-build
 echo "#########################################################################################";
 echo "build dir - changed directory";
-echo "#########################################################################################";
 
 echo "#########################################################################################";
 echo "running cmake";
-echo "#########################################################################################";
 sudo cmake -DCMAKE_INSTALL_PREFIX=/opt/$NAME/root-install -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_LINKER=ld -Dall=ON $SOURCE
 
 sudo make -j$CORES
@@ -144,10 +116,9 @@ cd /usr/local/bin
 sudo ln -sv /opt/$NAME/root-install/bin/*
 
 #Change the lines if using different shell than bash (such as zsh)
-if [ $SHELL == "bash" ]; then 
+if [ $SHELL == "bash" ]; then
 echo "#########################################################################################";
 echo "Using bash";
-echo "#########################################################################################";
 cat <<EOT >> ~/.bashrc
 #
 #ROOT sourcing
